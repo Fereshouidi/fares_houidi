3532ede5766b73d82db3ccc8cb698d5c47be6fdc
fares <fereshouidi298@gmail.com>
2024-11-20 14:33:56 +0100

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ import './styles/icon.css';
 import './styles/media.css';
 import './scripts/desin.js';
 import React from "react";
-// import { CldImage } from 'next-cloudinary';
+import { CldImage } from 'next-cloudinary';
 import { getAllProjects } from "./crud.mjs";
-//import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 // import { mouseEnter } from "./scripts/nav_icon.js";
 
