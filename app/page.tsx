9e72a78015d983a599732bbdbfdb5c171601730d
fares <fereshouidi298@gmail.com>
2024-11-21 12:51:37 +0100

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
     }
     const handelNavSectionExist = () => {
         setNavIconClicked(false);
-        setNavSecgtionHover(false).
+        setNavSecgtionHover(false)
         setNavSectionExist(false);
     }
   
